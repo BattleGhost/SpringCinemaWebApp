c64294d7f07541ae6c5bbe6f80b39165d3cd4265
Artur Safonov <assassinsedsed@gmail.com>
2021-10-13 00:31:17 +0300

    Locale implemented

--- src/main/java/com/example/springcinemawebapp/config/LocaleConfig.java
+++ src/main/java/com/example/springcinemawebapp/config/LocaleConfig.java
@@ -1,19 +1,27 @@
 package com.example.springcinemawebapp.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        return new CookieLocaleResolver();
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 
     @Bean
@@ -21,6 +29,23 @@ public class LocaleConfig implements WebMvcConfigurer {
         LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
         localeInterceptor.setParamName("lang");
         return localeInterceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
     }
 
     @Override
--- src/main/java/com/example/springcinemawebapp/dto/UserDTO.java
+++ src/main/java/com/example/springcinemawebapp/dto/UserDTO.java
@@ -9,31 +9,41 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDTO {
-    // TODO: 07.10.2021 MESSAGES (message = ...)
 
-    @NotEmpty
-    @NotNull
-    @Size(min = 2, max = 30)
+    // regex ^\p{Lu}\p{Ll}*([`'-]\p{Lu}\p{Ll}*)*$ for names like I-O
+    // regex ^\p{Lu}\p{Ll}+([`'-]\p{Lu}\p{Ll}+)*$ for names like Fu-Fo <--
+    public static final String realNameRegex = "^\\p{Lu}\\p{Ll}+([`'-]\\p{Lu}\\p{Ll}+)*$";
+
+    // regex with look-aheads for password that must contain 1 uppercase, 1 lowercase, 1 number, may contain any symbols
+    public static final String passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{3,}$";
+
+    @Pattern(regexp = realNameRegex, message = "{message.wrong.format.name.first}")
+    @NotEmpty(message = "{message.wrong.value.empty}")
+    @NotNull(message = "{message.wrong.value.null}")
+    @Size(min = 2, max = 30, message = "{message.wrong.size.name.first}")
     private String firstName;
 
-    @NotEmpty
-    @NotNull
-    @Size(min = 2, max = 30)
+    @Pattern(regexp = realNameRegex, message = "{message.wrong.format.name.last}")
+    @NotEmpty(message = "{message.wrong.value.empty}")
+    @NotNull(message = "{message.wrong.value.null}")
+    @Size(min = 2, max = 30, message = "{message.wrong.size.name.last}")
     private String lastName;
 
-    @NotEmpty
-    @NotNull
-    @Email
+    @NotEmpty(message = "{message.wrong.value.empty}")
+    @NotNull(message = "{message.wrong.value.null}")
+    @Email(message = "{message.wrong.format.email}")
     private String email;
 
-    @NotEmpty
-    @NotNull
-    @Size(min = 5, max = 30)
+    @Pattern(regexp = passwordRegex, message = "{message.wrong.format.password}")
+    @NotEmpty(message = "{message.wrong.value.empty}")
+    @NotNull(message = "{message.wrong.value.null}")
+    @Size(min = 5, max = 30, message = "{message.wrong.size.password}")
     private String password;
 }
